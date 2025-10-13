d99d26f39e8768063eba897c7bdf3ae1b6ad1ede
Shayan Khan <shayankhan@192.168.1.9>
2025-10-13 17:50:42 +0500

    Done with the Login Screen

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/widgets/reuseable_text_form_field.dart';
 
@@ -18,97 +19,127 @@ class LoginPage extends StatelessWidget {
             child: BackButton(),
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(18.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Instagram',
-                style: Theme.of(context).textTheme.displayLarge,
-              ),
-              SizedBox(height: deviceHeight * .05),
-              ReuseableTextFormField(hintText: 'Email'),
-              SizedBox(height: deviceHeight * .024),
-              ReuseableTextFormField(hintText: 'Password'),
-              SizedBox(height: deviceHeight * .020),
-              Align(
-                alignment: AlignmentGeometry.bottomRight,
-                child: InkWell(
-                  onTap: () {},
+        body: SingleChildScrollView(
+          child: Padding(
+            padding:  EdgeInsets.symmetric(vertical: deviceHeight * .05, horizontal: deviceHeight * .018),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Instagram',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                SizedBox(height: deviceHeight * .09),
+                ReuseableTextFormField(hintText: 'Email'),
+                SizedBox(height: deviceHeight * .024),
+                ReuseableTextFormField(hintText: 'Password'),
+                SizedBox(height: deviceHeight * .020),
+                Align(
+                  alignment: AlignmentGeometry.bottomRight,
+                  child: InkWell(
+                    onTap: () {},
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: deviceHeight * .024),
+                ElevatedButton(
+                  onPressed: () {},
+          
+                  style: ButtonStyle(
+                    shape: WidgetStatePropertyAll(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadiusGeometry.circular(8),
+                      ),
+                    ),
+                    minimumSize: WidgetStatePropertyAll<Size>(
+                      Size(deviceWidth * 0.915, deviceHeight * 0.054),
+                    ),
+                    backgroundColor: WidgetStatePropertyAll<Color>(
+                      Colors.blueAccent,
+                    ),
+                  ),
                   child: Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: deviceHeight * .024),
-              ElevatedButton(
-                onPressed: () {},
-
-                style: ButtonStyle(
-                  shape: WidgetStatePropertyAll(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadiusGeometry.circular(8),
-                    ),
-                  ),
-                  minimumSize: WidgetStatePropertyAll<Size>(
-                    Size(deviceWidth * 0.915, deviceHeight * 0.054),
-                  ),
-                  backgroundColor: WidgetStatePropertyAll<Color>(
-                    Colors.blueAccent,
-                  ),
-                ),
-                child: Text(
-                  "Log In",
-                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                    color: Colors.white,
-                    fontSize: 16,
-                  ),
-                ),
-              ),
-              SizedBox(height: deviceHeight * .05),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.facebook, color: Colors.blueAccent),
-                  SizedBox(width: deviceWidth * .025),
-                  Text(
-                    "Log in with Facebook",
+                    "Log In",
                     style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                      color: Colors.blueAccent,
+                      color: Colors.white,
                       fontSize: 16,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ],
-              ),
-              SizedBox(height: deviceHeight * .05),
-
-              Row(
-                children: [
-                  Container(width: 132, height: 0.3, color: Colors.grey),
-                  Spacer(),
-                  Text(
-                    "OR",
-                    style: Theme.of(context).textTheme.labelSmall!.copyWith(
-                      color: Colors.grey,
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
+                ),
+                SizedBox(height: deviceHeight * .05),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.facebook, color: Colors.blueAccent),
+                    SizedBox(width: deviceWidth * .025),
+                    InkWell(
+                      onTap: () {
+                        if(kDebugMode){
+                          print("Log in with Facebook");
+                        }
+                      },
+                      child: Text(
+                        "Log in with Facebook",
+                        style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                          color: Colors.blueAccent,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: deviceHeight * .05),
+          
+                Row(
+                  children: [
+                    Container(width: 132, height: 0.3, color: Colors.grey),
+                    Spacer(),
+                    Text(
+                      "OR",
+                      style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                        color: Colors.grey,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    Container(width: 132, height: 0.3, color: Colors.grey),
+                  ],
+                ),
+                SizedBox(height: deviceHeight * .05),
+                InkWell(
+                  onTap: () {
+                    if (kDebugMode) {
+                      print('Sign up');
+                    }
+                  },
+                  child: RichText(
+                    text: TextSpan(
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      children: [
+                        TextSpan(text: "Don't have an account ? "),
+                        TextSpan(
+                          text: "Sign up.",
+                          style: TextStyle(
+                            color: Colors.blueAccent,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Spacer(),
-                  Container(width: 132, height: 0.3, color: Colors.grey),
-                ],
-              ),
-              SizedBox(height: deviceHeight * .05),
-              
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
